64df13afaf8a138ede08daed6ffa48633cce309b
Sam Thomas <samuel.thomas@qubit.com>
2016-12-22 17:03:34 -0500

    For gavin

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (res, req) => {
-  res.send('Hello Omari')
+  res.send(`I honestly don't know what is going on, sorry gavin`)
 }
 
 app.listen(3000)
-
